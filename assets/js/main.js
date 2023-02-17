2ea8b619954e6fa522d30fdb61110b1ce51b5e8a
AndreaGazz <info.andreagazzaniga@gmail.com>
2023-02-17 14:01:27 +0100

    added imgs - removed js slides

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,7 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .gallery__text, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .images, .contact__input',{interval: 200}); 
@@ -81,3 +81,46 @@ sr.reveal('.skills__data, .images, .contact__input',{interval: 200});
  document.querySelector('#image-viewer .close').addEventListener('click', () => {
      imageViewer.style.display = 'none'
  })
+ /*$('.arrow-next').click(function() {
+    var currentSlide = $('.active-slide'),
+      nextSlide = currentSlide.next(),
+      currentDot = $('.active-dot'),
+      nextDot = currentDot.next();
+  
+    if (nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+      nextDot = $('.dot').first();
+    }
+  
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+  
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+  });
+  
+  $('.arrow-prev').click(function() {
+    var currentSlide = $('.active-slide'),
+      prevSlide = currentSlide.prev(),
+      currentDot = $('.active-dot'),
+      prevDot = currentDot.prev();
+  
+    if (prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    }
+  
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
+  
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+  });
+  
+  // this bit will resize the sliders height to make it responsive
+  $(window).on('load resize', function() {
+    var x = $('.active-slide img').height() + "px";
+  
+    $('.slider').css('min-height', x);
+    $('p').text(x);
+  });*/
